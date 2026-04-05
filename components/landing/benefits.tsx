00bbeeb6ba4849e4ab1c30218905198847c79728
David <davidmorras2@gmail.com>
2026-04-05 11:12:07 +0200

    Cambio porque elegirnos

--- components/landing/benefits.tsx
+++ components/landing/benefits.tsx
@@ -30,7 +30,7 @@ export function Benefits() {
             Tu rendimiento es nuestra prioridad
           </h2>
         </div>
-        <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
           {benefits.map((benefit, index) => (
             <div 
               key={index}
